d3c2223d1c293c041017eae33eb09409d38afa61
pantazis <pantazisvastardis@yahoo.com>
2020-02-10 13:05:51 +0200

    sussesful compile

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ const mix = require('laravel-mix');
 
 mix.options({
    processCssUrls: false 
-});
+}).setPublicPath('./')
+.sourceMaps()
+.version();
 
-mix.js('./resources/assets/js/homepage.js','./public/assets/js/homepage.js').sourceMaps(true, 'source-map').version();
+mix.js('./resources/assets/js/homepage.js','./public/assets/js/homepage.js');
