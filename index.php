18f8ae1af3ef9671cc20e00647ec029b56b0e05d
pantazis <pantazisvastardis@yahoo.com>
2020-02-12 12:03:48 +0200

    aaa

--- index.php
+++ index.php
@@ -21,6 +21,9 @@ echo '.'.$obj->$filename;
 
 
     <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i&display=swap&subset=greek-ext,latin-ext"
+        rel="stylesheet">
 
 
     <link href="<?php mix('/public/assets/css/home_page.css');?>" rel="stylesheet">
@@ -33,11 +36,45 @@ echo '.'.$obj->$filename;
     <div class="main_body">
         <div class="container">
             <div class="row">
-                <?php for ($x = 0; $x <= 10; $x++) { ?>
+                <div class="col-md-12">
+                    <div class="text_con">
+                        <h1 class="h2">Πίνακας ελέγχου</h1>
+                    </div>
+                </div>
                 <div class="col-md-4">
-                <div class="text_con">aaaa</div> </div>
-                <?php } ?>
-              
+                    <div class="text_con">
+                        <h2 class="h3">pantazis vastardis</h2>
+                        <p>pantazisvastardis@gmail.com <a href="">(επαλήθευση)</a>
+                            <p>
+                                +30.6932968500
+                            </p>
+                            <a href="">αλλαγή στοιχείων & ρυθμίσεων</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="text_con">
+                    <?php for ($x = 0; $x <= 2; $x++) { ?>
+                        <div class="listrow">
+                            <h2 class="h3">
+                                ΥΠΟΛΟΙΠΟ FORTH
+                            </h2>
+                            <p class="num">
+                                $47.928,31
+                            </p>
+                            <p>
+                                Τιμές:com: $8,95net: $15,55org: $8,95cc: $24,95tv: $39,95
+                            </p>
+                            <p>
+                                Συνολικά domains: 6
+                            </p>
+                        </div>
+                    <?php } ?>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
