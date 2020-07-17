d9aa2b08a0faa8a21d8d97e3c041536390c1c4ac
pantazis <pantazisvastardis@yahoo.com>
2020-07-17 11:50:25 +0300

    add styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,8 +9,8 @@ mix.options({
 mix.js('./resources/assets/js/admin_home.js','./public/assets/js/admin_home.js')
 .sass('resources/assets/sass/admin_home.scss','public/assets/css');
 
-mix.js('./resources/assets/js/table_pages.js','./public/assets/js/table_pages.js')
-.sass('resources/assets/sass/table_pages.scss','public/assets/css');
+//mix.js('./resources/assets/js/table_pages.js','./public/assets/js/table_pages.js')
+//.sass('resources/assets/sass/table_pages.scss','public/assets/css');
 
 
 
