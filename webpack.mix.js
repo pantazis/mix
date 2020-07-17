3471ff56ff9df7127c96a816934a5de142fdb12a
pantazis <pantazisvastardis@yahoo.com>
2020-07-17 15:29:44 +0300

    new js added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,29 @@
 const mix = require('laravel-mix');
+const uglifyJsFunctions = require('./uglify-js-functions');
+let combinefiles = uglifyJsFunctions.combinefiles;
+let watchChanges = uglifyJsFunctions.watchChanges;
+const fileConectionsArr = uglifyJsFunctions.fileConectionsArr;
+const createCssFiles  = uglifyJsFunctions.createCssFiles ;
+compileJs = process.argv.find(element => element == '--js');
 
-mix.options({
-   processCssUrls: false 
-}).setPublicPath('./')
-.sourceMaps(true, 'source-map')
-.version();
 
-mix.js('./resources/assets/js/admin_home.js','./public/assets/js/admin_home.js')
-.sass('resources/assets/sass/admin_home.scss','public/assets/css');
 
-//mix.js('./resources/assets/js/table_pages.js','./public/assets/js/table_pages.js')
-//.sass('resources/assets/sass/table_pages.scss','public/assets/css');
+watchChanges(fileConectionsArr);
+
+
+
+
+createCssFiles('public/scss/style.scss', 'public/css');
+
+
+
+
+
+
+
+
+
+
 
 
 
