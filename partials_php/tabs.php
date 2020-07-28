33c72b1e411f139763ecfd9708832efe56975ed4
pantazis <pantazisvastardis@yahoo.com>
2020-07-28 13:25:49 +0300

    test

--- partials_php/tabs.php
+++ partials_php/tabs.php
@@ -5,16 +5,21 @@
 
 <div class="col-lg-12">
 <div class="row">
+<div class="col-lg-12">
+<div class="text_con main_cat title">
+<h1 class="h2">Πίνακας ελέγχου</h1>
+<a href="" class="icon"><img src="public/assets/img/info.svg" alt=""></a>
+</div>
+</div>
+</div>
+<div class="row">
 <div class="col s12 p-0">
 <ul class="tabs tabs-transparent">
-<li class="tab col s3"><a class="active" href="#test1">Test 1</a></li>
-<li class="tab col s3"><a  href="#test2">Test 2</a></li>
-<li class="tab col s3"><a href="#test3">Test 3</a></li>
+<li class="tab col s3"><a class="active" href="#view1">Test 1</a></li>
+<li class="tab col s3"><a  href="#view2">Test 2</a></li>
+<li class="tab col s3"><a href="#view3">Test 3</a></li>
 </ul>
 </div>
-<!--<div id="test1" class="col s12">Test 1</div>
-<div id="test2" class="col s12">Test 2</div>
-<div id="test3" class="col s12">Test 3</div>-->
 </div>
 </div>
 </div>
