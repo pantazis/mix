670adc75b318c4715fb00c1994bd127fc5f0f371
pantazis <pantazisvastardis@yahoo.com>
2020-08-21 12:08:26 +0300

    dfdf

--- partials_php/filters_icon.php
+++ partials_php/filters_icon.php
@@ -1,67 +1,70 @@
-<svg id="eo8gwptf0t31"
+<svg id="filters"
 	xmlns="http://www.w3.org/2000/svg"
 	xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 18 19" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
 	<style>
-	.open svg #eo8gwptf0t33_to {
-  animation: eo8gwptf0t33_to__to 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t33_ts {
-  animation: eo8gwptf0t33_ts__ts 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t34 {
-  animation: eo8gwptf0t34__sz 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t34_to {
-  animation: eo8gwptf0t34_to__to 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t34_tr {
-  animation: eo8gwptf0t34_tr__tr 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t35 {
-  animation: eo8gwptf0t35__sz 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t35_to {
-  animation: eo8gwptf0t35_to__to 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t35_tr {
-  animation: eo8gwptf0t35_tr__tr 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t36_to {
-  animation: eo8gwptf0t36_to__to 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t36_ts {
-  animation: eo8gwptf0t36_ts__ts 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t37_to {
-  animation: eo8gwptf0t37_to__to 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t37_ts {
-  animation: eo8gwptf0t37_ts__ts 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t38_to {
-  animation: eo8gwptf0t38_to__to 1000ms linear 1 normal forwards;
-}
-.open svg #eo8gwptf0t38_ts {
-  animation: eo8gwptf0t38_ts__ts 1000ms linear 1 normal forwards;
-}
+	svg{
+		width:50px;
+	}
+	svg:hover #line1_to {
+  animation: line1_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line1_ts {
+  animation: line1_ts__ts 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line2 {
+  animation: line2__sz 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line2_to {
+  animation: line2_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line2_tr {
+  animation: line2_tr__tr 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line3 {
+  animation: line3__sz 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line3_to {
+  animation: line3_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #line3_tr {
+  animation: line3_tr__tr 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #circle1_to {
+  animation: circle1_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #circle1_ts {
+  animation: circle1_ts__ts 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #circle2_to {
+  animation: circle2_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #circle2_ts {
+  animation: circle2_ts__ts 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #circle3_to {
+  animation: circle3_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg:hover #circle3_ts {
+  animation: circle3_ts__ts 1000ms linear 1 normal forwards;
+ transition: all 1s; }
 /***************************************************************************/
-svg #eo8gwptf0t36_to  {
-  animation: eo8gwptf0t236_to__to 1000ms linear 1 normal forwards;
+svg #circle1_to  {
+  animation: filter_move1_to__to 1000ms linear 1 normal forwards;
   animation-delay: 1s;
-}
-svg #eo8gwptf0t37_to  {
-  animation: eo8gwptf0t237_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg #circle2_to  {
+  animation: filter_move2_to__to 1000ms linear 1 normal forwards;
   animation-delay: 1s;
-}
-svg #eo8gwptf0t38_to {
-  animation: eo8gwptf0t238_to__to 1000ms linear 1 normal forwards;
+ transition: all 1s; }
+svg #circle3_to {
+  animation: filter_move3_to__to 1000ms linear 1 normal forwards;
   animation-delay: 1s;
-}
+ transition: all 1s; }
 
 
 
 
-@keyframes eo8gwptf0t33_to__to {
+@keyframes line1_to__to {
 	0% {
 		transform: translate(0px, 0px)
 	}
@@ -72,7 +75,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t33_ts__ts {
+@keyframes line1_ts__ts {
 
 	0% {
 		transform: scale(1, 1)
@@ -84,7 +87,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t34__sz {
+@keyframes line2__sz {
 
 	0% {
 		width: 18px;
@@ -98,7 +101,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t34_to__to {
+@keyframes line2_to__to {
 
 	0% {
 		transform: translate(9px, 9.500000px)
@@ -110,7 +113,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t34_tr__tr {
+@keyframes line2_tr__tr {
 
 	0% {
 		transform: rotate(0deg)
@@ -122,7 +125,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t35__sz {
+@keyframes line3__sz {
     0% {
 		width: 18px;
 		height: 1px
@@ -135,7 +138,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t35_to__to {
+@keyframes line3_to__to {
     0% {
 		transform: translate(9px, 16.717514px)
 	}
@@ -146,7 +149,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t35_tr__tr {
+@keyframes line3_tr__tr {
     0% {
 		transform: rotate(0deg)
 	}
@@ -157,7 +160,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t36_to__to {
+@keyframes circle1_to__to {
     0% {
 		transform: translate(0px, 0px)
 	}
@@ -168,7 +171,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t36_ts__ts {
+@keyframes circle1_ts__ts {
     0% {
 		transform: scale(1, 1)
 	}
@@ -179,7 +182,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t37_to__to {
+@keyframes circle2_to__to {
 	0% {
 		transform: translate(0px, 0px)
 	}
@@ -190,7 +193,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t37_ts__ts {
+@keyframes circle2_ts__ts {
     0% {
 		transform: scale(1, 1)
 	}
@@ -201,7 +204,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t38_to__to {
+@keyframes circle3_to__to {
 	0% {
 		transform: translate(0px, 0px)
 	}
@@ -212,7 +215,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t38_ts__ts {
+@keyframes circle3_ts__ts {
     0% {
 		transform: scale(1, 1)
 	}
@@ -225,7 +228,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t236_to__to {
+@keyframes filter_move1_to__to {
 	0% {
 		transform: translate(0px, 0px)
 	}
@@ -243,7 +246,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t237_to__to {
+@keyframes filter_move2_to__to {
 	0% {
 		transform: translate(0px, 0px)
 	}
@@ -261,7 +264,7 @@ svg #eo8gwptf0t38_to {
 
 
 
-@keyframes eo8gwptf0t238_to__to {
+@keyframes filter_move3_to__to {
 	0% {
 		transform: translate(0px, 0px)
 	}
@@ -279,35 +282,35 @@ svg #eo8gwptf0t38_to {
 
 
 	</style>
-	<g id="eo8gwptf0t32">
-		<g id="eo8gwptf0t33_to" transform="translate(0,0)">
-			<g id="eo8gwptf0t33_ts" transform="scale(1,1)">
-				<rect id="eo8gwptf0t33" width="18" height="1" rx="0" ry="0" transform="translate(0,2)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
+	<g id="group">
+		<g id="line1_to" transform="translate(0,0)">
+			<g id="line1_ts" transform="scale(1,1)">
+				<rect id="line1" width="18" height="1" rx="0" ry="0" transform="translate(0,2)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
 			</g>
 		</g>
-		<g id="eo8gwptf0t34_to" transform="translate(9,9.500000)">
-			<g id="eo8gwptf0t34_tr" transform="rotate(0)">
-				<rect id="eo8gwptf0t34" width="18" height="1" rx="0" ry="0" transform="translate(-9,-0.500000)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
+		<g id="line2_to" transform="translate(9,9.500000)">
+			<g id="line2_tr" transform="rotate(0)">
+				<rect id="line2" width="18" height="1" rx="0" ry="0" transform="translate(-9,-0.500000)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
 			</g>
 		</g>
-		<g id="eo8gwptf0t35_to" transform="translate(9,16.717514)">
-			<g id="eo8gwptf0t35_tr" transform="rotate(0)">
-				<rect id="eo8gwptf0t35" width="18" height="1" rx="0" ry="0" transform="translate(-9,-0.500000)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
+		<g id="line3_to" transform="translate(9,16.717514)">
+			<g id="line3_tr" transform="rotate(0)">
+				<rect id="line3" width="18" height="1" rx="0" ry="0" transform="translate(-9,-0.500000)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
 			</g>
 		</g>
-		<g id="eo8gwptf0t36_to" transform="translate(0,0)">
-			<g id="eo8gwptf0t36_ts" transform="scale(1,1)">
-				<path id="eo8gwptf0t36" d="M5,2.500000C5,3.900000,3.900000,5,2.500000,5C1.100000,5,0,3.900000,0,2.500000C0,1.100000,1.100000,0,2.500000,0C3.900000,0,5,1.100000,5,2.500000Z" transform="translate(0,0)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
+		<g id="circle1_to" transform="translate(0,0)">
+			<g id="circle1_ts" transform="scale(1,1)">
+				<path id="circle1" d="M5,2.500000C5,3.900000,3.900000,5,2.500000,5C1.100000,5,0,3.900000,0,2.500000C0,1.100000,1.100000,0,2.500000,0C3.900000,0,5,1.100000,5,2.500000Z" transform="translate(0,0)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
 			</g>
 		</g>
-		<g id="eo8gwptf0t37_to" transform="translate(0,0)">
-			<g id="eo8gwptf0t37_ts" transform="scale(1,1)">
-				<path id="eo8gwptf0t37" d="M16,9.500000C16,10.900000,14.900000,12,13.500000,12C12.100000,12,11,10.900000,11,9.500000C11,8.100000,12.100000,7,13.500000,7C14.900000,7,16,8.100000,16,9.500000Z" transform="translate(0,0)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
+		<g id="circle2_to" transform="translate(0,0)">
+			<g id="circle2_ts" transform="scale(1,1)">
+				<path id="circle2" d="M16,9.500000C16,10.900000,14.900000,12,13.500000,12C12.100000,12,11,10.900000,11,9.500000C11,8.100000,12.100000,7,13.500000,7C14.900000,7,16,8.100000,16,9.500000Z" transform="translate(0,0)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
 			</g>
 		</g>
-		<g id="eo8gwptf0t38_to" transform="translate(0,0)">
-			<g id="eo8gwptf0t38_ts" transform="scale(1,1)">
-				<path id="eo8gwptf0t38" d="M9,16.500000C9,17.900000,7.900000,19,6.500000,19C5.100000,19,4,17.900000,4,16.500000C4,15.100000,5.100000,14,6.500000,14C7.900000,14,9,15.100000,9,16.500000Z" transform="translate(0,0)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
+		<g id="circle3_to" transform="translate(0,0)">
+			<g id="circle3_ts" transform="scale(1,1)">
+				<path id="circle3" d="M9,16.500000C9,17.900000,7.900000,19,6.500000,19C5.100000,19,4,17.900000,4,16.500000C4,15.100000,5.100000,14,6.500000,14C7.900000,14,9,15.100000,9,16.500000Z" transform="translate(0,0)" fill="rgb(57,67,78)" stroke="none" stroke-width="1"/>
 			</g>
 		</g>
 	</g>
